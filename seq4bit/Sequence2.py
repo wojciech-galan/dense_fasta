ca039b5f2dc0f5fe3b8af479aa9d1471f50f0530
wojciech-galan <wojciech.galan@gmail.com>
2017-06-19 09:04:42 +0200

    to_sharedmem - many processes

--- seq4bit/Sequence2.py
+++ seq4bit/Sequence2.py
@@ -10,12 +10,21 @@ from Sequence import representation, rev_representation
 # assert it works in 64bit environment
 assert sys.maxsize == 2**63 - 1
 
-def to_sharedmem(sequence, odd=False):
+def to_sharedmem(sequence, length, odd=False, processes=4): #ToDO change processes to sth more usefull
     arr = sharedmem.empty(math.ceil(len(sequence)/2), dtype='u1')
-    for x in range(0, len(sequence)//2*2, 2):# lerret in sequence:
-        arr[x/2] = (representation[sequence[x]]<<4) + representation[sequence[x+1]]
+    with sharedmem.MapReduce(np=2) as pool:
+        chunksize = int(length/processes//2*2)
+        def work(i):
+            print (i)
+            if (i + chunksize) < length:
+                end = i + chunksize
+            else:
+                end = length
+            for x in range(i, end//2*2, 2):
+                arr[x/2] = (representation[sequence[x]]<<4) + representation[sequence[x+1]]
+        r = pool.map(work, range(0, length, chunksize))
     if odd:
-        arr[x/2+1] = representation[sequence[x+2]]<<4
+        arr[length//2] = representation[sequence[length-1]]<<4
     return arr
 
 def to_string(array, odd=False):
@@ -46,13 +55,13 @@ class Sequence(object):
     def load_string(self, string):
         self.length = len(string)
         self.odd = self.length % 2
-        self.seq = to_sharedmem(string, self.odd)
+        self.seq = to_sharedmem(string, self.length, self.odd)
 
 
 if __name__ == '__main__':
     import timeit
-    #seq = 'AGATA'*20000
-    #print(str(Sequence(seq)))
+    seq = 'AGATANNNA'
+    print(str(Sequence(seq)))
     for mnoznik in (5000, 10000, 20000, 40000, 80000, 200000):
         print(mnoznik)
         # print(timeit.timeit('str(Sequence(seq, to_int_int, to_string_int))',
