af69d8006fe3de165001c4142a90cb32ef240715
wojciech-galan <wojciech.galan@gmail.com>
2017-06-20 19:08:39 +0200

    compression and decompression works for multiple processes

--- seq4bit/Sequence2.py
+++ seq4bit/Sequence2.py
@@ -10,31 +10,46 @@ from Sequence import representation, rev_representation
 # assert environment is 64bit environment
 assert sys.maxsize == 2**63 - 1
 
-def to_sharedmem(sequence, length, odd=False, processes=4): #ToDO change processes to sth more usefull
+
+def to_sharedmem(sequence, length, odd=False, processes=sharedmem.cpu_count()):
     arr = sharedmem.empty(math.ceil(len(sequence)/2), dtype='u1')
     with sharedmem.MapReduce(np=processes) as pool:
-        chunksize = int(length/processes//2*2)
+        chunksize = int(length/processes//2*2) or 1
+
         def work(i):
-            print (i)
             if (i + chunksize) < length:
                 end = i + chunksize
             else:
                 end = length
             for x in range(i, end//2*2, 2):
                 arr[x/2] = (representation[sequence[x]]<<4) + representation[sequence[x+1]]
+
         r = pool.map(work, range(0, length, chunksize))
     if odd:
         arr[length//2] = representation[sequence[length-1]]<<4
     return arr
 
-def to_string(array, odd=False):
-    out_list = []
-    for x in range(array.shape[0]):
-        out_list.append(rev_representation[array[x] >> 4])
-        out_list.append(rev_representation[array[x] & 0x0F])
+
+def to_string(array, odd=False, processes=sharedmem.cpu_count()):
+    arr_size = array.shape[0]
+    out_arr = sharedmem.empty([arr_size*2], dtype='|S1')
+    with sharedmem.MapReduce(np=processes) as pool:
+        chunksize = int(arr_size / processes) or 1
+
+        def work(i):
+            if (i + chunksize) < arr_size:
+                end = i + chunksize
+            else:
+                end = arr_size
+            for x in range(i, end):
+                out_arr[x*2] = rev_representation[array[x] >> 4]
+                out_arr[x*2+1] = rev_representation[array[x] & 0x0F]
+
+        r = pool.map(work, range(0, arr_size, chunksize))
     if odd:
-        out_list.pop()
-    return ''.join(out_list)
+        return str(out_arr[:-1].tostring().decode())
+    return str(out_arr.tostring().decode())
+
 
 class Sequence(object):
 
@@ -64,10 +79,6 @@ if __name__ == '__main__':
     print(str(Sequence(seq)))
     for mnoznik in (5000, 10000, 20000, 40000, 80000, 200000):
         print(mnoznik)
-        # print(timeit.timeit('str(Sequence(seq, to_int_int, to_string_int))',
-        #                     setup="from Sequence import Sequence, to_int_int, to_string_int; seq = 'AGATA'*%d"%mnoznik, number=1))
-        # print(timeit.timeit('str(Sequence(seq, to_int_gmpy2, to_string_gmpy2))',
-        #                     setup="from Sequence import Sequence, to_int_gmpy2, to_string_gmpy2; seq = 'AGATA'*%d"%mnoznik, number=1))
         print(timeit.timeit('str(Sequence(seq))',
                             setup="from __main__ import Sequence; seq = 'AGATA'*%d" % mnoznik,
-                            number=1))
+                            number=10))
