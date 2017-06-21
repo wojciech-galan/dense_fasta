5d3a6257f90033beaccabd26b2b2a38e0f4dfbaa
wojciech-galan <wojciech.galan@gmail.com>
2017-06-21 12:16:12 +0200

    Sequence class made abstract

--- seq4bit/Sequence.py
+++ seq4bit/Sequence.py
@@ -1,11 +1,24 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+import abc
 
-class Sequence(object):
+class Sequence(abc.ABC):
 
     def __init__(self):
         super().__init__()
 
     def __len__(self):
         return self.length
+
+    @abc.abstractmethod
+    def load_string(self, string):pass
+
+    @abc.abstractmethod
+    def __str__(self):pass
+
+
+if __name__ == '__main__':
+    # should raise an exception
+    s = Sequence()
+
