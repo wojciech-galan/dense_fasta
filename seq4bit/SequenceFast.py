3db69271fd21df14aed9479ee84cdd490ce66367
wojciech-galan <wojciech.galan@gmail.com>
2017-06-21 15:24:40 +0200

    fastest implementation on 1 core

--- seq4bit/SequenceFast.py
+++ seq4bit/SequenceFast.py
@@ -2,19 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import psutil
+import gmpy2
 from Sequence import Sequence
 from bit4rep import representation, rev_representation
 from SequenceShort import to_int, to_string
 
 
 def to_list(string, length, chunksize):
-    #print(length, chunksize, [to_int(string[x:x+chunksize]) for x in range(0, length, chunksize)])
     return [to_int(string[x:x+chunksize]) for x in range(0, length, chunksize)]
 
 
 def to_string_(a_list):
     return ''.join([to_string(element) for element in a_list])
-
 
 class SequenceFast(Sequence):
 
@@ -37,7 +36,7 @@ if __name__ == '__main__':
     print(SequenceFast(seq, 2))
     seq *= 10000
     assert str(SequenceFast(seq)) == seq
-    for x in [32, 64, 128, 256]:
+    for x in [8, 16, 32, 64, 128, 256, 512]:
         for mnoznik in (625, 1250, 2500, 5000, 10000, 20000, 40000, 80000, 200000):
             print(x, mnoznik*5, timeit.timeit('str(SequenceFast(seq, chunksize=%d))'%x,
                                 setup="from __main__ import SequenceFast; seq = 'AGATA'*%d"%mnoznik, number=100))
